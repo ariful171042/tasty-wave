96217b031b1f57551f15eaae26958a2d3553bb24
ariful171042 <arif171042@gmail.com>
2023-06-03 13:01:23 +0600

    hero complete

---
+++ components/Overlay.tsx
@@ -0,0 +1,7 @@
+const Overlay = () => {
+  return (
+    <div className="absolute top-0 left-0 w-full h-full z-[1] bg-gradient-to-t from-black/75 to-transparent"></div>
+  );
+};
+
+export default Overlay;
--- components/hero.tsx
+++ components/hero.tsx
@@ -2,18 +2,31 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/pagination";
 import { data } from "@/data/sliderData";
 
-import { Navigation } from "swiper";
+import { Navigation, Pagination, Autoplay, Parallax } from "swiper";
 import Image from "next/image";
 import Link from "next/link";
+import Overlay from "./Overlay";
 
 const HeroSection = () => {
   return (
     <section className="w-full h-[75vh]">
       <Swiper
+        parallax={true}
+        grabCursor={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        speed={700}
         navigation={true}
-        modules={[Navigation]}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Navigation, Pagination, Autoplay, Parallax]}
         className="mySwiper w-full h-full"
       >
         {data.map((item) => (
@@ -27,13 +40,22 @@ const HeroSection = () => {
                 className="w-full h-full object-cover"
                 priority
               />
-              <div className="absolute top-0 left-0 w-full h-full z-[1] bg-gradient-to-t from-black/75 to-transparent"></div>
+              <Overlay />
               <div className=" absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-center z-[2] space-y-5">
-                <h1 className="text-8xl font-semibold">{item.headline}</h1>
-                <p className="text-lg ">{item.paragraph}</p>
-                <Link href="/products" className="btn btn-accent">
-                  {item.cta}
-                </Link>
+                <h1
+                  data-swiper-parallax="-300%"
+                  className=" text-5xl lg:text-8xl font-semibold"
+                >
+                  {item.headline}
+                </h1>
+                <p data-swiper-parallax="-400%" className="text-lg ">
+                  {item.paragraph}
+                </p>
+                <div data-swiper-parallax="-500%">
+                  <Link href="/products" className="btn btn-accent">
+                    {item.cta}
+                  </Link>
+                </div>
               </div>
             </div>
           </SwiperSlide>
